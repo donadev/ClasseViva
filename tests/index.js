3d6247828f96c60b9a818aa5e9621223d4fb0073
Luigi Donadel <luigi.donadel@gmail.com>
2016-03-24 19:41:06 +0100

    added basic tests... just one fails.. need to figure out why server gives null response

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,56 @@
-var boot = require('../app').boot,
-    shutdown = require('../app').shutdown,
-    port = require('../app').port,
-    superagent = require('superagent'),
-    expect = require('expect.js');
+    var app = require("../app"),
+        request = require('supertest'),
+        expect = require('expect.js');
+    var agent = request.agent(app);
+    //those are my ClasseViva credentials... you can enter on my ClasseViva account, I have nothing to hide ;)
+    //replace if you want with your credentials
+    var TEST_SCHOOL = "pnit0003",
+        TEST_USER = "S724407K",
+        TEST_PW = "Prova1234";
 
-    describe('server', function () {
-      before(boot);
-      after(shutdown);
+    describe("auth controller", function() {
+          it("should give bad credentials error", function(next) {
+              agent.post("/auth/login")
+                   .field("school", TEST_SCHOOL)
+                   .field("username", TEST_USER + "x")
+                   .field("password", TEST_PW)
+                   .expect('Content-Type', /json/)
+                   .expect(200, {
+                       success: false,
+                       error: "Credenziali errate"
+                   }, next);
+          });
+          it("should give bad credentials error", function(next) {
+              agent.post("/auth/login")
+                   .field("school", TEST_SCHOOL)
+                   .field("username", TEST_USER)
+                   .field("password", TEST_PW + "x")
+                   .expect('Content-Type', /json/)
+                   .expect(200, {
+                       success: false,
+                       error: "Credenziali errate"
+                   }, next);
+          });
+          it("should give bad credentials error", function(next) {
+              agent.post("/auth/login")
+                   .field("school", TEST_SCHOOL + "x")
+                   .field("username", TEST_USER)
+                   .field("password", TEST_PW)
+                   .expect('Content-Type', /json/)
+                   .expect(200, {
+                       success: false,
+                       error: "Credenziali errate"
+                   }, next);
+          });
+          it("should give me the phpsessid", function(next) {
+              agent.post("/auth/login")
+                   .field("school", TEST_SCHOOL)
+                   .field("username", TEST_USER)
+                   .field("password", TEST_PW)
+                   .expect('Content-Type', /json/)
+                   .expect(200, function(body) {
+                       return body.success == true && !_.isEmpty(body.data);
+                   })
+                   .end(next);
+          });
     });
