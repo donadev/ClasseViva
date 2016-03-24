61d079e41ad4013adf160a084d8250b51c11e7db
Luigi Donadel <luigi.donadel@gmail.com>
2016-03-24 06:45:34 +0100

    added auth route for authentication handling purposes

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var Requester = require("../services/Requester");
+var router = express.Router();
+var cheerio = require("cheerio");
+router.post('/login', (req, res, next) => {
+  
+});
+
+module.exports = router;
