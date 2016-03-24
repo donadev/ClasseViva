b5b486b6850fe4b16d40ba87261795901aabb3cc
Luigi Donadel <luigi.donadel@gmail.com>
2016-03-24 19:02:54 +0100

    added working login api (to integrate with unit tests)

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var Requester = require("../services/Requester");
 var router = express.Router();
-var cheerio = require("cheerio");
 router.post('/login', (req, res, next) => {
-  
+  var school = req.body.school;
+  var code = req.body.student_code;
+  var password = req.body.password;
+  Requester.login(school, code, password).then((response) => {
+      if(response.status == 302) res.success(response.cookies["PHPSESSID"]);
+      else next("Credenziali errate");
+  }).catch((response) => {
+      next(response.error);
+  });
 });
 
 module.exports = router;
