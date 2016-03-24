8a4ad324c0b98c7d09904a0742f9f44cb30eedd9
Luigi Donadel <luigi.donadel@gmail.com>
2016-03-24 20:03:34 +0100

    added passing unit tests for login feature

--- tests/index.js
+++ tests/index.js
@@ -9,47 +9,53 @@
         TEST_PW = "Prova1234";
 
     describe("auth controller", function() {
-          it("should give bad credentials error", function(next) {
+          it("should give bad credentials error (bad student code)", function(next) {
               agent.post("/auth/login")
-                   .field("school", TEST_SCHOOL)
-                   .field("username", TEST_USER + "x")
-                   .field("password", TEST_PW)
+                   .type("form")
+                   .send({"school": TEST_SCHOOL})
+                   .send({"student_code": TEST_USER + "x"})
+                   .send({"password": TEST_PW})
                    .expect('Content-Type', /json/)
                    .expect(200, {
                        success: false,
                        error: "Credenziali errate"
                    }, next);
           });
-          it("should give bad credentials error", function(next) {
+          it("should give bad credentials error (bad password)", function(next) {
               agent.post("/auth/login")
-                   .field("school", TEST_SCHOOL)
-                   .field("username", TEST_USER)
-                   .field("password", TEST_PW + "x")
+                   .type("form")
+                   .send({"school": TEST_SCHOOL})
+                   .send({"student_code": TEST_USER})
+                   .send({"password": TEST_PW + "x"})
                    .expect('Content-Type', /json/)
                    .expect(200, {
                        success: false,
                        error: "Credenziali errate"
                    }, next);
           });
-          it("should give bad credentials error", function(next) {
+          it("should give bad credentials error (bad school code)", function(next) {
               agent.post("/auth/login")
-                   .field("school", TEST_SCHOOL + "x")
-                   .field("username", TEST_USER)
-                   .field("password", TEST_PW)
+                   .type("form")
+                   .send({"school": TEST_SCHOOL + "x"})
+                   .send({"student_code": TEST_USER})
+                   .send({"password": TEST_PW})
                    .expect('Content-Type', /json/)
                    .expect(200, {
                        success: false,
                        error: "Credenziali errate"
                    }, next);
           });
-          it("should give me the phpsessid", function(next) {
+          it("Should send successfully the phpsessid", function(next) {
               agent.post("/auth/login")
-                   .field("school", TEST_SCHOOL)
-                   .field("username", TEST_USER)
-                   .field("password", TEST_PW)
+                   .type("form")
+                   .send({"school": TEST_SCHOOL})
+                   .send({"student_code": TEST_USER})
+                   .send({"password": TEST_PW})
                    .expect('Content-Type', /json/)
-                   .expect(200, function(body) {
-                       return body.success == true && !_.isEmpty(body.data);
+                   .expect(200)
+                   .expect(function(response) {
+                       var body = response.body;
+                       return body.success && !_.isEmpty(body.data);
                    })
                    .end(next);
           });
